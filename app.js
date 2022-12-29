9878a189aa221d4e463dbb162201344605967fdd
Ebin Soby <ebin.soby@alertdriving.com>
2022-12-29 11:57:15 -0500

    directives, root-components, modifiers, side-effects

--- app.js
+++ app.js
@@ -2,12 +2,27 @@ Vue.createApp({
   data() {
     return {
       firstName: "Ebin",
-      lastName:"Soby",
+      middleName: "",
+      lastName: "Soby",
+      url: "https://www.google.com/",
+      age: 22,
     };
   },
-  methods:{
-    fullName(){
-      return `${this.firstName} ${this.lastName}`
-    }
-  }
+  methods: {
+    increment() {
+      this.age++;
+    },
+    updateMiddleName(event) {
+      this.middleName = event.target.value;
+    },
+    updateLastName(msg, event) {
+      console.log(msg);
+      this.lastName = event.target.value;
+    },
+  },
+  computed: {
+    fullName() {
+      return `${this.firstName} ${this.middleName} ${this.lastName}`;
+    },
+  },
 }).mount("#app");
