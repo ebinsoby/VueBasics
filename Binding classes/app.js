354b14351c0e6ec37ef8702f0edf5fdcdd7dc573
Ebin Soby <mailebinsoby@gmail.com>
2022-12-29 16:30:47 -0500

    binding styles

--- Binding classes/app.js
+++ Binding classes/app.js
@@ -2,6 +2,15 @@ let vm = Vue.createApp({
   data() {
     return {
       isPurple: false,
+      selectedTextColor: '',
+      size: 150,
     };
+  },
+  computed: {
+    circle_classes() {
+      return {
+        purple: this.isPurple,
+      };
+    },
   },
 }).mount("#app");
